ef39c018a02f51d0e7a02164a387136d3aedfd9d
Pratik Dashore <pratikdashore@hotmail.com>
2017-04-12 12:37:15 +0530

    changes for demo

--- app/1.ts
+++ app/1.ts
@@ -6,18 +6,40 @@ console.log('Hello TypeScript');
 
 /////////////////////// Premitive data types  ///////////////////////////////////
 
+let aString = 'Pratik';
+
+var Aboolen: boolean;
+
+var aNumber: number;
+
+var aANy: any;
+var aArray: number[];
+
+aArray = [123];
 
 
 
 /////////////////////////// Object types  /////////////////////
+
+var person: { name: string, age: number } = { name: 'Pratik', age: 25 };
 
 
 // Functions, class, module, interface, and literal types
 
 
 
+
 ////////////////////// functions /////////////////////////////////
 
+function getNames(arr: { name: string, age: number }[]) {
+    return arr.map((item) => {
+        return item.name;
+    });
+}
+
+var persons = [person, person];
+var names = getNames(persons);
+console.log(names);
 
 
 
@@ -26,11 +48,31 @@ console.log('Hello TypeScript');
 //////////////////////// ambient declaration ///////////////////
 
 
+declare var document: Document;
 
 
 
 
 ///////////////////////////// Type Definition files /////////////
+
+
+/////////////////////////////// types ///////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ app/2.ts
@@ -0,0 +1,54 @@
+
+
+interface IPerson {
+    name: string;
+    age: number;
+    gender: string;
+    height?: number;
+}
+
+interface IStudent extends IPerson {
+    rollNumber: number;
+}
+
+class Student implements IStudent {
+    name: string;
+    age: number;
+    gender: string;
+    height?: number;
+    rollNumber: number;
+}
+
+class AllStudent extends Student {
+
+}
+
+
+
+
+
+class Person implements IPerson {
+    public gender: string;
+    constructor(public name: string, public age: number) {
+
+    }
+
+    private _height: number;
+    get height() {
+        return this._height;
+    }
+
+    set height(value: number) {
+        this._height = value;
+    }
+
+    getDetails() {
+        return this;
+    }
+
+}
+
+
+var person12: Person = new Person('Pratik', 25);
+person12.height = 176;
+
